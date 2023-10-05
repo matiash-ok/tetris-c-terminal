bf0effad7c03d23dad9be91baac13ccd0245da73
matiash-ok <mathy0095@gmail.com>
2023-10-05 12:57:43 -0300

    add tetroblock it can move and fall

--- tetris.c
+++ tetris.c
@@ -21,29 +21,13 @@ typedef struct Tetroid {
     int mode;
 }tetro;
 
-void create_tetro_form(int mode){
-    switch (mode) {
-        case 1:
-            gameBoard[BOARD_WIDTH/2][BOARD_HEIGHT] = 3;
-            gameBoard[BOARD_WIDTH/2][BOARD_HEIGHT-1] = 2;
-            gameBoard[BOARD_WIDTH/2][BOARD_HEIGHT-2] = 2;
-            gameBoard[BOARD_WIDTH/2][BOARD_HEIGHT+1] = 2;
-
-  //      case 2: create_tetro_form(2);;
-  //      case 3: create_tetro_form(3);
-  //      case 4: create_tetro_form(4);
-  //      case 5: create_tetro_form(5);
-  //      case 6: create_tetro_form(6);
-  //      case 7: create_tetro_form(7);
-    }
-}
 tetro tetroid;
+tetro tetroid1;
+tetro tetroid2;
+tetro tetroid3;
+
 void clearTerminal() {
     refresh();
-}
-
-void monitor_screen(char * message){
-mvprintw(7,12,"%s",message);
 }
 
 void create_tetro(){
@@ -51,7 +35,28 @@ void create_tetro(){
     int rand_num = 1 ;
     switch (rand_num) {
         case 1:
-            create_tetro_form(1);
+            // lineblock
+            // main tetroid
+            gameBoard[BOARD_WIDTH/2][BOARD_HEIGHT] = 3;
+            tetroid.x = BOARD_WIDTH / 2;
+            tetroid.y = BOARD_HEIGHT ;
+            tetroid.stopped = false;
+
+            //other tetroid
+            gameBoard[BOARD_WIDTH/2][BOARD_HEIGHT-1] = 2;
+            tetroid1.x = BOARD_WIDTH / 2;
+            tetroid1.y = BOARD_HEIGHT - 1 ;
+            tetroid1.stopped = false;
+
+            gameBoard[BOARD_WIDTH/2][BOARD_HEIGHT-2] = 4;
+            tetroid2.x = BOARD_WIDTH / 2;
+            tetroid2.y = BOARD_HEIGHT - 2 ;
+            tetroid2.stopped = false;
+
+            gameBoard[BOARD_WIDTH/2][BOARD_HEIGHT+1] = 5;
+            tetroid3.x = BOARD_WIDTH / 2;
+            tetroid3.y = BOARD_HEIGHT + 2 ;
+            tetroid3.stopped = false;
             break;
       //  case 2: create_tetro_form(2);
       //  case 3: create_tetro_form(3);
@@ -61,10 +66,34 @@ void create_tetro(){
       //  case 7: create_tetro_form(7);
     }
     should_create_tetro = false;
-    tetroid.x = BOARD_WIDTH / 2;
-    tetroid.y = BOARD_HEIGHT - 1 ;
-    tetroid.stopped = false;
-}
+  }
+
+bool tetroblock_can_move(int mov){
+    return (
+       (tetroid.x  + mov >= 0 && tetroid.x  + mov < BOARD_WIDTH) &&
+       (tetroid1.x + mov >= 0 && tetroid1.x + mov < BOARD_WIDTH) &&
+       (tetroid2.x + mov >= 0 && tetroid2.x + mov < BOARD_WIDTH) &&
+       (tetroid3.x + mov >= 0 && tetroid3.x + mov < BOARD_WIDTH) &&
+
+        (gameBoard[tetroid.x+mov][tetroid.y] == 0 ||
+         gameBoard[tetroid.x+mov][tetroid.y] == 2 ||
+         gameBoard[tetroid.x+mov][tetroid.y] == 3 ) &&
+
+        (gameBoard[tetroid1.x+mov][tetroid1.y] == 0 ||
+         gameBoard[tetroid1.x+mov][tetroid1.y] == 2 ||
+         gameBoard[tetroid1.x+mov][tetroid1.y] == 3 ) &&
+
+        (gameBoard[tetroid2.x+mov][tetroid2.y] == 0 ||
+         gameBoard[tetroid2.x+mov][tetroid2.y] == 2 ||
+         gameBoard[tetroid2.x+mov][tetroid2.y] == 3 ) &&
+
+        (gameBoard[tetroid3.x+mov][tetroid3.y] == 0 ||
+         gameBoard[tetroid3.x+mov][tetroid3.y] == 2 ||
+         gameBoard[tetroid3.x+mov][tetroid3.y] == 3 )
+
+    );
+};
+
 void drawGameBoard() {
     clearTerminal();
 
@@ -82,7 +111,10 @@ void drawGameBoard() {
             printw(" %d \n",y); // Newline at the end of each row
         }
     }
-    mvprintw(5,14,"tetro X : %d tetro Y : %d",tetroid.x ,tetroid.y);
+    mvprintw(5,14,"3 tetro  X : %d tetro Y : %d",tetroid.x  ,tetroid.y);
+    mvprintw(6,14,"2 tetro1 X : %d tetro Y : %d",tetroid1.x ,tetroid1.y);
+    mvprintw(7,14,"4 tetro2 X : %d tetro Y : %d",tetroid2.x ,tetroid2.y);
+    mvprintw(8,14,"5 tetro3 X : %d tetro Y : %d",tetroid3.x ,tetroid3.y);
 }
 
 void clean_board(){
@@ -95,13 +127,24 @@ void clean_board(){
     }
 }
 void move_tetro(int mov){
-   int tetroY = tetroid.y ;
-   int tetroX = tetroid.x ;
 
-    if(tetroX + mov < BOARD_WIDTH && tetroX + mov >= 0 && gameBoard[tetroX+mov][tetroY]== 0 ){
-        gameBoard[tetroX][tetroY] = 0;
-        gameBoard[tetroX + mov][tetroY] = 3;
-        tetroid.x = tetroX + mov;
+    if(tetroblock_can_move(mov)){
+        gameBoard[tetroid.x][tetroid.y] = 0;
+        gameBoard[tetroid.x + mov][tetroid.y] = 3;
+        tetroid.x = tetroid.x + mov;
+
+        gameBoard[tetroid1.x][tetroid1.y] = 0;
+        gameBoard[tetroid1.x + mov][tetroid1.y] = 2;
+        tetroid1.x = tetroid1.x + mov;
+
+        gameBoard[tetroid2.x][tetroid2.y] = 0;
+        gameBoard[tetroid2.x + mov][tetroid2.y] = 4;
+        tetroid2.x = tetroid2.x + mov;
+
+        gameBoard[tetroid3.x][tetroid3.y] = 0;
+        gameBoard[tetroid3.x + mov][tetroid3.y] = 5;
+        tetroid3.x = tetroid3.x + mov;
+
         drawGameBoard();
     }
 
@@ -112,28 +155,25 @@ void gravity(){
            if(gameBoard[x][y] == 0){
 
                 if(y+1<= BOARD_HEIGHT){
-                   if(gameBoard[x][y+1] == 3){
-                       gameBoard[x][y] = 3;
-                   }
-                   if(gameBoard[x][y+1] == 2){
-                       gameBoard[x][y] = 2;
-                   }
+                       gameBoard[x][y] = gameBoard[x][y+1];
                 }
            }
 
-            if(gameBoard[x][y] == 3){
-                if(gameBoard[x][y+1] == 0 && gameBoard[x][y-1] == 3){
-                    gameBoard[x][y] = 0;
-                }
-                if(gameBoard[x][y+1] == 2){
-                    gameBoard[x][y] = 2;
-                }
-                tetroid.y = y-1;
-            }
-
-            if(gameBoard[x][y] == 2){
-                if(y+1 < BOARD_HEIGHT + 3){
+            if(gameBoard[x][y] == 3 ||
+               gameBoard[x][y] == 2 ||
+               gameBoard[x][y] == 4 ||
+               gameBoard[x][y] == 5
+            ){
+                if(y+1 < BOARD_HEIGHT + 3 && gameBoard[x][y-1] != 1){
+                    switch(gameBoard[x][y]){
+                        case 3: tetroid.y = y-1  ;break;
+                        case 2: tetroid1.y = y-1 ;break;
+                        case 4: tetroid2.y = y-1 ;break;
+                        case 5: tetroid3.y = y-1 ;break;
+                    }
                     gameBoard[x][y] = gameBoard[x][y+1];
+                }else{
+                    tetroid.stopped = true;
                 }
             }
         }

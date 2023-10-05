97b7375b51e1f7c0aa73dd7994a964f291e8cc96
matiash-ok <mathy0095@gmail.com>
2023-10-04 23:52:06 -0300

    add line form

--- forms.c
+++ forms.c
@@ -1,6 +1,28 @@
-#include <string.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
+typedef struct Tetroid {
+    int x ;
+    int y ;
+    bool stopped;
+    int mode;
+}tetro;
 
-int main(){
-    puts("Hello World");
+int create_tetro_form(int mode,int widthX,int heightY, int board[widthX][heightY] ){
+    switch (mode) {
+        case 1:
+            board[widthX/2][heightY] = 3;
+            board[widthX/2][heightY-1] = 2;
+            board[widthX/2][heightY-2] = 2;
+            board[widthX/2][heightY+1] = 2;
+            return widthX / 2;
+
+  //      case 2: create_tetro_form(2);;
+  //      case 3: create_tetro_form(3);
+  //      case 4: create_tetro_form(4);
+  //      case 5: create_tetro_form(5);
+  //      case 6: create_tetro_form(6);
+  //      case 7: create_tetro_form(7);
+    }
+    return 2;
 }
--- tetris.c
+++ tetris.c
@@ -7,23 +7,39 @@
 #include <time.h>
 #include <ncursesw/ncurses.h>
 
+
 #define BOARD_WIDTH 10
 #define BOARD_HEIGHT 20
 
-int gameBoard[BOARD_WIDTH][BOARD_HEIGHT] = {0};
+int gameBoard[BOARD_WIDTH][BOARD_HEIGHT + 3] = {0};
 bool should_create_tetro = false;
 
 typedef struct Tetroid {
     int x ;
     int y ;
     bool stopped;
+    int mode;
 }tetro;
 
+void create_tetro_form(int mode){
+    switch (mode) {
+        case 1:
+            gameBoard[BOARD_WIDTH/2][BOARD_HEIGHT] = 3;
+            gameBoard[BOARD_WIDTH/2][BOARD_HEIGHT-1] = 2;
+            gameBoard[BOARD_WIDTH/2][BOARD_HEIGHT-2] = 2;
+            gameBoard[BOARD_WIDTH/2][BOARD_HEIGHT+1] = 2;
+
+  //      case 2: create_tetro_form(2);;
+  //      case 3: create_tetro_form(3);
+  //      case 4: create_tetro_form(4);
+  //      case 5: create_tetro_form(5);
+  //      case 6: create_tetro_form(6);
+  //      case 7: create_tetro_form(7);
+    }
+}
 tetro tetroid;
 void clearTerminal() {
     refresh();
-    //system("clear");
-    //printw("\e[1;1H\e[2J"); // ANSI escape code to clear the terminal
 }
 
 void monitor_screen(char * message){
@@ -31,16 +47,28 @@ mvprintw(7,12,"%s",message);
 }
 
 void create_tetro(){
-    gameBoard[2][BOARD_HEIGHT -1] = 3;
+   // int rand_num = (rand() % 7) + 1;
+    int rand_num = 1 ;
+    switch (rand_num) {
+        case 1:
+            create_tetro_form(1);
+            break;
+      //  case 2: create_tetro_form(2);
+      //  case 3: create_tetro_form(3);
+      //  case 4: create_tetro_form(4);
+      //  case 5: create_tetro_form(5);
+      //  case 6: create_tetro_form(6);
+      //  case 7: create_tetro_form(7);
+    }
     should_create_tetro = false;
-    tetroid.x = 2 ;
+    tetroid.x = BOARD_WIDTH / 2;
     tetroid.y = BOARD_HEIGHT - 1 ;
     tetroid.stopped = false;
 }
 void drawGameBoard() {
     clearTerminal();
 
-    for (int y = BOARD_HEIGHT - 1; y >= 0; y--) {
+    for (int y = BOARD_HEIGHT -1; y >= 0; y--) {
         for (int x = 0; x < BOARD_WIDTH; x++) {
                mvprintw(BOARD_HEIGHT-y-1,x,"%d",gameBoard[x][y]); // Occupied cell
         }
@@ -79,22 +107,34 @@ void move_tetro(int mov){
 
 }
 void gravity(){
-   for (int y = 0; y < BOARD_HEIGHT ; y++) {
+   for (int y = 0; y < BOARD_HEIGHT+3 ; y++) {
         for (int x = 0; x < BOARD_WIDTH; x++) {
            if(gameBoard[x][y] == 0){
 
-                if(y+1< BOARD_HEIGHT && gameBoard[x][y+1] == 3){
-                    gameBoard[x][y] = 3;
-                    monitor_screen("move down");
+                if(y+1<= BOARD_HEIGHT){
+                   if(gameBoard[x][y+1] == 3){
+                       gameBoard[x][y] = 3;
+                   }
+                   if(gameBoard[x][y+1] == 2){
+                       gameBoard[x][y] = 2;
+                   }
                 }
            }
 
             if(gameBoard[x][y] == 3){
-                if(gameBoard[x][y-1] == 3){
+                if(gameBoard[x][y+1] == 0 && gameBoard[x][y-1] == 3){
                     gameBoard[x][y] = 0;
-                    tetroid.y = y-1;
                 }
-              //  if(gameBoard[next_mov][y+1] == 1) gameBoard[x][y] = 1;
+                if(gameBoard[x][y+1] == 2){
+                    gameBoard[x][y] = 2;
+                }
+                tetroid.y = y-1;
+            }
+
+            if(gameBoard[x][y] == 2){
+                if(y+1 < BOARD_HEIGHT + 3){
+                    gameBoard[x][y] = gameBoard[x][y+1];
+                }
             }
         }
     }
@@ -102,6 +142,7 @@ void gravity(){
 }
 
 int main() {
+    srand(time(NULL));
     initscr();
     cbreak();
     start_color();
